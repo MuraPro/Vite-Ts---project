4cf0d2fb3d8b4cd490bcae6a65ac84ef9588946a
MuraPro <chalanamdo@gmail.com>
2024-11-05 19:10:16 +0900

    first commit

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import classes from './AboutPage.module.scss';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <p className={classes.text}>ABOUT PAGE</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import classes from './MainPage.module.scss';
+
+const MainPage = () => {
+  return (
+    <div>
+      <p className={classes.text}>MainPage</p>
+    </div>
+  );
+};
+
+export default MainPage;
