c37c4023e2999984c667b94b8c40f8d7ab7bf8fd
MuraPro <chalanamdo@gmail.com>
2024-11-15 20:49:15 +0900

    test github actions 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { CollapseProvider } from "app/providers/CollapseProvider";
 import { ErrorBoundary } from "app/providers/ErrorBoundary";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
 import "./shared/config/i18n/i18n";
 import "./app/styles/index.scss";
 
