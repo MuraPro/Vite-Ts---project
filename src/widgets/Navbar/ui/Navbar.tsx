878d0b616fa517bdef4ac8d32b0f899aba9c29be
MuraPro <chalanamdo@gmail.com>
2024-11-17 18:32:19 +0900

    fix linting alerts

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,4 @@
 import { BugButton } from "app/providers/ErrorBoundary";
-import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
@@ -11,7 +10,6 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
   return (
     <nav
       className={classNames(cls.header__navbar, {}, [
