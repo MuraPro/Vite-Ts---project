1952216286f1840dc43071085a7f073ff4ed83d5
MuraPro <chalanamdo@gmail.com>
2024-11-26 09:49:12 +0900

    test husky

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,11 @@
 module.exports = {
+  "*.{ts,tsx,js,jsx}": ["npm run fix", "npm run test:unit"],
+  "*.css": ["npm run fix:css"],
+  "src/**/*.{html,ts,tsx,js,jsx}": [
+    "npm run build",
+    "npm run sb",
+    "npm run test:ui",
+  ],
   "!**/.github/**": [],
   "!**/.husky/**": [],
 };
