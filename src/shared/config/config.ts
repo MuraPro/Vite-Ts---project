13ba17163a42df47df0824e8a32f998d137bb7f2
MuraPro <chalanamdo@gmail.com>
2024-12-07 20:15:07 +0900

    add api url for production

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -2,5 +2,4 @@ export const configEnv = {
   apiUrl: process.env.VITE_API_URL || "http://localhost:8000/",
   isDev: process.env.VITE_IS_DEV,
   mode: process.env.VITE_MODE || "development",
-  isDevMode: process.env.NODE_ENV === "development",
 };
