285a737a38dc55083a1b09af988e9a191453fce6
MuraPro <chalanamdo@gmail.com>
2025-01-29 12:24:51 +0900

    add toggle features for component

--- scripts/remove-feature.ts
+++ scripts/remove-feature.ts
@@ -1,7 +1,10 @@
-import { Node, Project, SyntaxKind } from 'ts-morph';
+import { JsxAttribute, Node, Project, SyntaxKind } from 'ts-morph';
 
 const removedFeatureName = process.argv[2]; // example isArticleEnabled
-const featureState = process.argv[3]; // example off\on
+const featureState = process.argv[3]; // example off/on
+
+const toggleFunctionName = 'toggleFeatures';
+const toggleComponentName = 'ToggleFeatures';
 
 if (!removedFeatureName) {
     throw new Error('Укажите название фича-флага');
@@ -15,64 +18,109 @@ if (featureState !== 'on' && featureState !== 'off') {
     throw new Error('Некорректное значение состояния фичи (on или off)');
 }
 
-const project = new Project({});
+const project = new Project({
+    tsConfigFilePath: 'tsconfig.json',
+});
 
-project.addSourceFilesAtPaths('src/**/*.ts');
-project.addSourceFilesAtPaths('src/**/*.tsx');
+project.addSourceFilesAtPaths('src/**/*.{ts,tsx}');
 
 const files = project.getSourceFiles();
 
 function isToggleFunction(node: Node) {
-    let isToggleFeatures = false;
-
-    node.forEachChild((child) => {
-        if (
-            child.isKind(SyntaxKind.Identifier) &&
-            child.getText() === 'toggleFeatures'
-        ) {
-            isToggleFeatures = true;
-        }
-    });
-
-    return isToggleFeatures;
+    return node
+        .getChildren()
+        .some(
+            (child) =>
+                child.isKind(SyntaxKind.Identifier) &&
+                child.getText() === toggleFunctionName,
+        );
 }
+
+function isToggleComponent(node: Node) {
+    const identifier = node.getFirstDescendantByKind(SyntaxKind.Identifier);
+    return identifier?.getText() === toggleComponentName;
+}
+
+const replaceToggleFunction = (node: Node) => {
+    const objectOptions = node.getFirstDescendantByKind(
+        SyntaxKind.ObjectLiteralExpression,
+    );
+    if (!objectOptions) return;
+
+    const offFunctionProperty = objectOptions.getProperty('off');
+    const onFunctionProperty = objectOptions.getProperty('on');
+    const featureNameProperty = objectOptions.getProperty('name');
+
+    const onFunction = onFunctionProperty?.getFirstDescendantByKind(
+        SyntaxKind.ArrowFunction,
+    );
+    const offFunction = offFunctionProperty?.getFirstDescendantByKind(
+        SyntaxKind.ArrowFunction,
+    );
+    const featureNameText = featureNameProperty
+        ?.getFirstDescendantByKind(SyntaxKind.StringLiteral)
+        ?.getLiteralText();
+
+    if (featureNameText !== removedFeatureName) return;
+
+    if (featureState === 'on' && onFunction) {
+        node.replaceWithText(onFunction.getBody().getText());
+    } else if (featureState === 'off' && offFunction) {
+        node.replaceWithText(offFunction.getBody().getText());
+    }
+};
+
+const getAttributeNodeByName = (
+    jsxAttributes: JsxAttribute[],
+    name: string,
+) => {
+    return jsxAttributes.find((node) => node.getNameNode()?.getText() === name); // Используем getNameNode()
+};
+
+const getReplacedComponent = (attribute?: JsxAttribute) => {
+    const valueNode = attribute
+        ?.getFirstDescendantByKind(SyntaxKind.JsxExpression)
+        ?.getExpression();
+    return valueNode ? valueNode.getText() : undefined;
+};
+
+const replaceComponent = (node: Node) => {
+    const attributes = node.getDescendantsOfKind(SyntaxKind.JsxAttribute);
+
+    const onAttribute = getAttributeNodeByName(attributes, 'on');
+    const offAttribute = getAttributeNodeByName(attributes, 'off');
+    const featureNameAttribute = getAttributeNodeByName(attributes, 'feature');
+
+    const featureNameText = featureNameAttribute
+        ?.getFirstDescendantByKind(SyntaxKind.StringLiteral)
+        ?.getLiteralText();
+    if (featureNameText !== removedFeatureName) return;
+
+    const offValue = getReplacedComponent(offAttribute);
+    const onValue = getReplacedComponent(onAttribute);
+
+    if (featureState === 'on' && onValue) {
+        node.replaceWithText(onValue);
+    } else if (featureState === 'off' && offValue) {
+        node.replaceWithText(offValue);
+    }
+};
 
 files.forEach((sourceFile) => {
     sourceFile.forEachDescendant((node) => {
         if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
-            const objectOptions = node.getFirstDescendantByKind(
-                SyntaxKind.ObjectLiteralExpression,
-            );
+            replaceToggleFunction(node);
+        }
 
-            if (!objectOptions) return;
-
-            const offFunctionProperty = objectOptions.getProperty('off');
-            const onFunctionProperty = objectOptions.getProperty('on');
-
-            const featureNameProperty = objectOptions.getProperty('name');
-
-            const onFunction = onFunctionProperty?.getFirstDescendantByKind(
-                SyntaxKind.ArrowFunction,
-            );
-            const offFunction = offFunctionProperty?.getFirstDescendantByKind(
-                SyntaxKind.ArrowFunction,
-            );
-            const featureName = featureNameProperty
-                ?.getFirstDescendantByKind(SyntaxKind.StringLiteral)
-                ?.getText()
-                .slice(1, -1);
-
-            if (featureName !== removedFeatureName) return;
-
-            if (featureState === 'on') {
-                node.replaceWithText(onFunction?.getBody().getText() ?? '');
-            }
-
-            if (featureState === 'off') {
-                node.replaceWithText(offFunction?.getBody().getText() ?? '');
-            }
+        if (
+            node.isKind(SyntaxKind.JsxSelfClosingElement) &&
+            isToggleComponent(node)
+        ) {
+            replaceComponent(node);
         }
     });
 });
 
-project.save();
+(async () => {
+    await project.save();
+})();
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -9,7 +9,7 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { toggleFeatures } from '@/shared/lib/features';
+import { ToggleFeatures } from '@/shared/lib/features';
 import { Card } from '@/shared/ui/Card';
 import { VStack } from '@/shared/ui/Stack';
 import { Page } from '@/widgets/Page';
@@ -35,12 +35,6 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         return null;
     }
 
-    const articleRatingCard = toggleFeatures({
-        name: 'isArticleRatingEnabled',
-        on: () => <ArticleRating articleId={id} />,
-        off: () => <Card>{t('Оценка статей скоро появится!')}</Card>,
-    });
-
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <Page
@@ -50,7 +44,15 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
                     <VStack gap="16" max>
                         <ArticleDetailsPageHeader />
                         <ArticleDetails id={id} />
-                        {articleRatingCard}
+                        <ToggleFeatures
+                            feature="isArticleRatingEnabled"
+                            on={<ArticleRating articleId={id} />}
+                            off={
+                                <Card>
+                                    {t('Оценка статей скоро появится!')}
+                                </Card>
+                            }
+                        />
                         <ArticleRecommendationsList />
                         <ArticleDetailsComments id={id} />
                     </VStack>
