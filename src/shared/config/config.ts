a28d351fa7354c88e6bb029adac8196cd9b7a211
MuraPro <chalanamdo@gmail.com>
2025-01-29 12:28:13 +0900

    turn on configEnv

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,9 +1,9 @@
-// export const configEnv = {
-//     apiUrl: process.env.VITE_API_URL || 'http://localhost:8000/',
-//     isDev: process.env.VITE_IS_DEV,
-//     mode: process.env.VITE_MODE || 'development',
-//     isDevMode: process.env.NODE_ENV === 'development',
-// };
+export const configEnv = {
+    apiUrl: process.env.VITE_API_URL || 'http://localhost:8000/',
+    isDev: process.env.VITE_IS_DEV,
+    mode: process.env.VITE_MODE || 'development',
+    isDevMode: process.env.NODE_ENV === 'development',
+};
 
 // export const configEnv = {
 //     // Используйте import.meta.env для получения значений из переменных окружения Vite
